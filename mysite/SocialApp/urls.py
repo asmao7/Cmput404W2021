f39bb266498aae259b934a18c190773f1edd92d0
soomyy <aaali@ualberta.ca>
2021-02-24 11:39:00 -0700

    added login and signup pages

--- mysite/SocialApp/urls.py
+++ mysite/SocialApp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home')
+  path('', views.home, name='home'),
+  path('index.html', views.home, name='home'),
+  path('', views.Login, name='Login'),  
+  path('Login.html', views.Login, name='Login'),
+   path('signup.html', views.signup, name='signup')   
 ]
--- mysite/SocialApp/views.py
+++ mysite/SocialApp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+def Login(request):
+    return render(request, 'Login.html', {})
+
+def signup(request):
+    return render(request, 'signup.html', {})
