fd49d7a8008a25139a85d4330e2dea03f7b9f8d6
soomyy <aaali@ualberta.ca>
2021-03-02 10:44:05 -0700

    updated views

--- mysite/SocialApp/views.py
+++ mysite/SocialApp/views.py
@@ -1,18 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+class UserRegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('login')
+
 def home(request):
     return render(request, 'home.html', {})
 
-def Login(request):
-    return render(request, 'Login.html', {})
-
-def signup(request):
-    return render(request, 'signup.html', {})
-
 def author(request):
     return render(request, 'author.html', {})
-
 
 def editProfile(request):
     return render(request, 'editProfile.html', {})
