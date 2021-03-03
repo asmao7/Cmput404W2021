31ea5e6064d00a8822f287ef8efab5794d450f06
soomyy <aaali@ualberta.ca>
2021-03-03 00:45:59 -0700

    updated urls

--- mysite/SocialApp/urls.py
+++ mysite/SocialApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 from .views import UserRegisterView
+from .views import HomeView, PostDetailView, AddPostView
 
 urlpatterns = [
   path('', views.home, name='home'),
@@ -9,9 +10,11 @@ urlpatterns = [
   #path('', views.Login, name='Login'),  
   #path('Login.html', views.Login, name='Login'),
   path('signup.html', UserRegisterView.as_view(), name="signup"),
-  path('author.html', views.author, name='author') ,
+  path('author.html', HomeView.as_view(), name="author"),
+  path('Details/<int:pk>', PostDetailView.as_view(), name="post-details"),
+  path('AddPost.html', AddPostView.as_view(), name="add_post"),
   path('editProfile.html', views.editProfile, name='editProfile'),
-  path('newPost.html', views.newPost, name='newPost'),
+  #path('newPost.html', views.newPost, name='newPost'),
   path('newMessage.html', views.newMessage, name='newMessage'),
   path('author/<str:author_id>/', views.AuthorEndpoint.as_view(), name='Author')
 ]
