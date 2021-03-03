45dd25408b96435351b87f8b20ab93c9164d9bfe
Robert MacGillivray <macgillivray.robert@gmail.com>
2021-03-02 17:50:39 -0700

    Forgot an Import

--- mysite/SocialApp/admin.py
+++ mysite/SocialApp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import TextPost, ImagePost, Author, PostCategory, Comment
 
 class DefaultAdmin(admin.ModelAdmin):
