5cbe63162c598045114a430ed2c7c3de66cff22d
Robert MacGillivray <macgillivray.robert@gmail.com>
2021-03-02 23:02:23 -0700

    First Post Endpoint Test

---
+++ mysite/SocialApp/utils.py
@@ -0,0 +1,48 @@
+from .models import Author, Post, TextPost, ImagePost
+
+
+def AuthorToJSON(author):
+    # Converts an Author object into JSON. Returns None if the author is bad
+    if not author:
+        return None
+    try:
+        json = {
+            "type":"author",
+            "id":author.url,
+            "host":author.host,
+            "displayName":author.display_name,
+            "url":author.url,
+            "github":author.github
+        }
+        return json
+    except:
+        return None
+
+
+# TODO: Fill out categories, count, size, comments
+def PostToJSON(post):
+    # Converts a Post object into JSON. Return None if the Post is bad
+    if not post:
+        return None
+    try:
+        json = {
+            "type":"post",
+            "title":post.title,
+            "id":post.url,
+            "source":post.source,
+            "origin":post.origin,
+            "description":post.description,
+            "contentType":post.content_type,
+            "content":post.content,
+            "author":AuthorToJSON(post.author),
+            "categories":"",
+            "count":0,
+            "size":0,
+            "comments":"",
+            "published":post.published,
+            "visibility":post.visibility,
+            "unlisted":post.unlisted
+        }
+        return json
+    except:
+        return None
--- mysite/SocialApp/views.py
+++ mysite/SocialApp/views.py
@@ -1,9 +1,13 @@
+import datetime
+
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from rest_framework.views import APIView
 
-from .models import Author
+from .models import Author, Post, TextPost, ImagePost
 from .admin import AuthorCreationForm
+
+from .utils import AuthorToJSON, PostToJSON
 
 from django.views import generic
 from django.urls import reverse_lazy
@@ -42,22 +46,17 @@ class AuthorEndpoint(APIView):
         if not author:
             return HttpResponse(status=404)
 
-        json = {
-            "type":"author",
-            "id":author.url,
-            "host":author.host,
-            "displayName":author.display_name,
-            "url":author.url,
-            "github":author.github
-        }
-        return JsonResponse(json)
+        return JsonResponse(AuthorToJSON(author))
 
     def post(self, request, *args, **kwargs):
-        author_id = kwargs.get('author_id', -1)
+        author_id = kwargs.get("author_id", -1)
         if author_id == -1:
             return HttpResponse(status=404)
 
-        author = Author.objects.get(pk=author_id)
+        try:
+            author = Author.objects.get(pk=author_id)
+        except:
+            return HttpResponse(status=400)
         if not author:
             return HttpResponse(status=404)
 
@@ -69,3 +68,137 @@ class AuthorEndpoint(APIView):
         author.save()
 
         return HttpResponse(status=200)
+
+
+class PostEndpoint(APIView):
+    def get(self, request, *args, **kwargs):
+        author_id = kwargs.get("author_id", -1)
+        if author_id == -1:
+            return HttpResponse(status=404)
+
+        try:
+            author = Author.objects.get(pk=author_id)
+        except:
+            return HttpResponse(status=400)
+        if not author:
+            return HttpResponse(status=404)
+
+        post_id = kwargs.get("post_id", -1)
+        if post_id == -1:
+            return HttpResponse(status=404)
+
+        post = None
+        try:
+            post = TextPost.objects.get(pk=post_id)
+        except:
+            try:
+                post = ImagePost.objects.get(pk=post_id)
+            except:
+                return HttpResponse(status=400)
+        if not post:
+            return HttpResponse(status=404)
+
+        return JsonResponse(PostToJSON(post))
+
+    # TODO: Make this authenticated
+    def post(self, request, *args, **kwargs):
+        author_id = kwargs.get("author_id", -1)
+        if author_id == -1:
+            return HttpResponse(status=404)
+
+        try:
+            author = Author.objects.get(pk=author_id)
+        except:
+            return HttpResponse(status=400)
+        if not author:
+            return HttpResponse(status=404)
+
+        post_id = kwargs.get("post_id", -1)
+        if post_id == -1:
+            return HttpResponse(status=404)
+
+        post = None
+        try:
+            post = TextPost.objects.get(pk=post_id)
+        except:
+            try:
+                post = ImagePost.objects.get(pk=post_id)
+            except:
+                return HttpResponse(status=400)
+        if not post:
+            return HttpResponse(status=404)
+
+        # TODO: Handle categories, choose proper visibility
+        jsonData = request.data
+        post.title = jsonData.get("title")
+        post.id = jsonData.get("id")
+        post.source = jsonData.get("source")
+        post.origin = jsonData.get("origin")
+        post.description = jsonData.get("description")
+        post.content_type = jsonData.get("contentType")
+        post.content = jsonData.get("content")
+        post.categories = None
+        post.published = datetime(jsonData.get("published"))
+        post.visibility = Post.Visibility.PUBLIC
+        post.unlisted = bool(jsonData.get("unlisted"))
+        post.save()
+
+        return HttpResponse(status=200)
+
+    def delete(self, request, *args, **kwargs):
+        author_id = kwargs.get("author_id", -1)
+        if author_id == -1:
+            return HttpResponse(status=404)
+
+        try:
+            author = Author.objects.get(pk=author_id)
+        except:
+            return HttpResponse(status=400)
+        if not author:
+            return HttpResponse(status=404)
+
+        post_id = kwargs.get("post_id", -1)
+        if post_id == -1:
+            return HttpResponse(status=404)
+
+        post = None
+        try:
+            post = TextPost.objects.get(pk=post_id)
+        except:
+            try:
+                post = ImagePost.objects.get(pk=post_id)
+            except:
+                return HttpResponse(status=400)
+        if not post:
+            return HttpResponse(status=404)
+
+        post.delete()
+
+        return HttpResponse(status=200)
+
+    # TODO: manage post creation based on content type
+    def put(self, request, *args, **kwargs):
+        author_id = kwargs.get("author_id", -1)
+        if author_id == -1:
+            return HttpResponse(status=404)
+
+        try:
+            author = Author.objects.get(pk=author_id)
+        except:
+            return HttpResponse(status=400)
+        if not author:
+            return HttpResponse(status=404)
+
+        post_id = kwargs.get("post_id", -1)
+        if post_id == -1:
+            return HttpResponse(status=404)
+
+        try:
+            jsonData = request.data
+            text_post = TextPost(id=post_id, title=jsonData.get("title"), url=jsonData.get("id"), source=jsonData.get("source"),
+                                 origin=jsonData.get("origin"), description=jsonData.get("description"), content_type=jsonData.get("contentType"),
+                                 author=author, published=datetime(jsonData.get("published")), visibility=Post.Visibility.PUBLIC, unlisted=bool(jsonData.get("unlisted"))
+            text_post.save()
+            return HttpResponse(status=200)
+        except:
+            return HttpResponse(status=500)
