401fc515155bd1785f402bd39f816db17a63d2e5
Robert MacGillivray <macgillivray.robert@gmail.com>
2021-03-04 02:00:13 -0700

    Update test_api.py
    - test bad paths for GET author endpoint

--- mysite/SocialApp/test_api.py
+++ mysite/SocialApp/test_api.py
@@ -87,6 +87,14 @@ class TestCases(TestCase):
         cls.assertEqual(response.status_code, status.HTTP_200_OK)
         cls.assertEqual(response.json(), AuthorToJSON(Author.objects.get(pk=cls.author_id_1)))
 
+        url = reverse("Author", kwargs={"author_id":"abc"})
+        response = client.get(url)
+        cls.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        url = reverse("Author")
+        response = client.get(url)
+        cls.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_author_post(cls):
         """
         Test the POST author/{AUTHOR_ID}/ endpoint
