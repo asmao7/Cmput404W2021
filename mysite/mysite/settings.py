0a0c7d6c2007e6ffe33954330811541e1728294a
Robert MacGillivray <macgillivray.robert@gmail.com>
2021-03-17 14:11:18 -0600

    Update settings.py
    - add markdownify app to settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'markdownify.apps.MarkdownifyConfig',
 ]
 
 MIDDLEWARE = [
