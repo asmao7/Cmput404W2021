81136a76aaf9242ec62098615ca04394e2364bd9
Anastasia Borissova <aborisso@ualberta.ca>
2021-03-29 22:17:17 -0400

    Styling for all posts page

--- mysite/SocialApp/templatetags/app_filters.py
+++ mysite/SocialApp/templatetags/app_filters.py
@@ -4,7 +4,6 @@ from SocialApp.models import LikedPost
 register = template.Library()
 
 @register.filter(name='liked_or_not')
-#@stringfilter
 def liked_or_not(value, arg):
 	liked = len(LikedPost.objects.filter(post_id=value, user_id=arg))
 	if liked == 0:
