381f651d4f67fa7c031690dbc4dbe055ade6e4e4
Robert MacGillivray <macgillivray.robert@gmail.com>
2021-04-11 02:36:44 -0600

    Custom Filter to Count Likes on Post

--- mysite/SocialApp/templatetags/app_filters.py
+++ mysite/SocialApp/templatetags/app_filters.py
@@ -4,9 +4,13 @@ from SocialApp.models import ObjectLike
 register = template.Library()
 
 @register.filter(name='liked_or_not')
-def liked_or_not(value, arg):
-	liked = len(ObjectLike.objects.filter(author_url=arg.url, object_url=value.url))
+def liked_or_not(post, author):
+	liked = len(ObjectLike.objects.filter(author_url=author.url, object_url=post.url))
 	if liked == 0:
 		return '♡'
 	else:
 		return '🖤'
+
+@register.filter(name='liked_count')
+def liked_count(post):
+	return len(ObjectLike.objects.filter(object_url=post.url))
