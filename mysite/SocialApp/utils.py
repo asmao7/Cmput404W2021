d1351c3be51cdf61fed2eb92e946c746ace7a561
Robert MacGillivray <macgillivray.robert@gmail.com>
2021-04-16 10:51:05 -0600

    Update utils.py
    - update comment

--- mysite/SocialApp/utils.py
+++ mysite/SocialApp/utils.py
@@ -130,6 +130,8 @@ def CommentToJSON(comment):
     if not comment.author_json:
         return None
 
+    # Used to fetch updated author representation to attach to a comment
+    # Was too slow in practice (heroku servers take a long time to wake up if they haven't had a request in a while), so our data can get stale now
     try:
         #basic_auth = GetURLBasicAuth(comment.author_url)
         #response = None
